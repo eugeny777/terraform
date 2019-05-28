7d52d71dd6f3459e68c9b2d7c7594802f6695bec
Yvheny Dan <50371909+eugeny777@users.noreply.github.com>
2019-05-28 14:47:31 +0300

    Update swarm_join.sh

--- swarm_join.sh
+++ swarm_join.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 yum install epel-release -y
 yum install jq -y
-TOKEN=$(curl -s swarm_master:2376/swarm |jq '."JoinTokens"."Worker"'|tr -d \"); docker swarm join  --token $TOKEN docker_master
+TOKEN=$(curl -s swarm_master:2376/swarm |jq '."JoinTokens"."Worker"'|tr -d \"); docker swarm join  --token $TOKEN swarm_master
